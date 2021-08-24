5ec9519288c86d34f532fd0c64745dbc24cb95b5
Morgan Hein <mhein@conquestcyber.com>
2021-08-24 09:02:27 -0700

    chore: remvoe node_modules from git

--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var web3_1 = __importDefault(require("web3"));
-var web3 = new web3_1.default(new web3_1.default.providers.HttpProvider("https://rinkeby.infura.io/"));
-console.log(web3);
