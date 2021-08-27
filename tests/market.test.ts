5d57a971e277ae295dab4590bd1801d7361d99ce
Morgan Hein <mhein@conquestcyber.com>
2021-08-27 10:17:42 -0700

    feat: .env.example with tests using .env

--- tests/market.test.ts
+++ tests/market.test.ts
@@ -1,10 +1,8 @@
 import { IBlockMarket, Web3Market } from "../src/market";
+import { ENV } from "../src/config";
 
 describe("OpenSea SDK", function () {
-  const market: IBlockMarket = new Web3Market(
-    "https://mainnet.infura.io/v3/0039582f375a4ed09d06e6948d1a5ac4",
-    ""
-  );
+  const market: IBlockMarket = new Web3Market(ENV.MarketHost, ENV.SourceWallet);
 
   const contractAddress = "0x495f947276749ce646f68ac8c248420045cb7b5e";
   const tokenId =
@@ -14,7 +12,6 @@ describe("OpenSea SDK", function () {
     try {
       const gas = await market.getGas();
       expect(gas).not.toBeNull();
-      //todo: not a great test, b/c it's possible that the gas price *is* actually 201...
       expect(gas).not.toEqual("-1");
       console.log(gas);
     } catch (error) {
@@ -34,10 +31,7 @@ describe("OpenSea SDK", function () {
 
   test("retrieving orders works", async () => {
     try {
-      const orders = await market.getOrders(
-        "0x14f03368b43e3a3d27d45f84fabd61cc07ea5da3",
-        "8284"
-      );
+      const orders = await market.getOrders(contractAddress, tokenId);
       expect(orders).not.toBeNull();
       expect(orders.length).toBeGreaterThan(0);
       console.log(orders);
